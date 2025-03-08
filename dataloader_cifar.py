e7c0447eb2dacf2ea78ea17f8384a85ddf1a611e
tuchan2002 <trananhtu171002@gmail.com>
2025-03-08 23:04:58 +0700

    push

---
+++ dataloader_cifar.py
@@ -0,0 +1,25 @@
+from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor, Resize, Compose
+from animal_dataset import AnimalDataset
+
+if __name__ == '__main__':
+  transform = Compose([
+    Resize((200,200)),
+    ToTensor()
+  ])
+  training_data = AnimalDataset(root="./data", train=True, transform=transform)
+  # training_data = CIFAR10(root="data", train=True, transform=ToTensor())
+  image, label = training_data.__getitem__(12345)
+
+  training_dataloader = DataLoader(
+    dataset=training_data,
+    batch_size=1000,
+    num_workers=4,
+    shuffle=True,
+    drop_last=False, 
+  )
+
+  for images, labels in training_dataloader:
+    print(images.shape)
+    print(labels)
